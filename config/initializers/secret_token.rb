b9ede066d4e3ff461e3bae75c2ba0576757a886b
bschmaltz <Ben.Schmaltz@careerbuilder.com>
2013-05-15 00:08:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeTreats::Application.config.secret_token = '539a83e5877d8d47a326bcf8182f5a31808ab55535c7b905e221616431176a2a4e6840fca82d286cf7f3107b66be2c8d50e140c473bdf792bc175210e63d765b'
