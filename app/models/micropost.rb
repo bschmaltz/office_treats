3310da70245911aa454b2a2c21c6a179b85224d5
bschmaltz <Ben.Schmaltz@careerbuilder.com>
2013-05-17 13:42:18 -0400

    added image upload, still has bugs

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content
+  attr_accessible :content, :name, :image, :remote_image_url
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true  
